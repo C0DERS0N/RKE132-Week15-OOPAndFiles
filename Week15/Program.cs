85fe2f0ccb8176414c9123b7e175ffb1143ba0b2
C0DERS0N <73357230+C0DERS0N@users.noreply.github.com>
2023-01-02 15:38:07 +0200

    Add files via upload

---
+++ Week15/Program.cs
@@ -0,0 +1,48 @@
+﻿List<Movie> myMovies = new List<Movie>();
+string[] data GetDataFromFile();
+
+foreach (string line in data)
+    {
+        string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+        Movie newMovie = new Movie(tempArray[0], tempArray[2]);
+        myMovies.Add(newMovie);
+    }
+
+    foreach (Movie movie in myMovies)
+    {
+        Console.WriteLine($"One of my favourite movies {movie.Title}  + was released in {movie.Year}.");
+    }
+  
+
+
+static string[] GetDataFromFile()
+{
+    string filePath = "C:\\Users\\source\\data\\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+
+class Movie
+{
+    
+    string title;
+    string year;
+
+
+   
+    public string Title
+    { 
+        get { return title; } 
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie (string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
